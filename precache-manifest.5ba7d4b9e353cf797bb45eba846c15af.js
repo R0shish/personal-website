a6baf4a612e4ed27782b3da049d63a5529eda51e
R0shish <92678280+R0shish@users.noreply.github.com>
2022-05-16 00:16:26 +0545

    Personal-Website

---
+++ precache-manifest.5ba7d4b9e353cf797bb45eba846c15af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c1ed7f08315eef70207ff91575a4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e15a57dfab5b4e53418",
+    "url": "/static/css/main.9eaaec2d.chunk.css"
+  },
+  {
+    "revision": "b8513799646ffd0b84dd",
+    "url": "/static/js/2.69d0df79.chunk.js"
+  },
+  {
+    "revision": "8d6ec8aa5e011f3a2f164cb9de3e4309",
+    "url": "/static/js/2.69d0df79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e15a57dfab5b4e53418",
+    "url": "/static/js/main.19a7a808.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "2e2ee372a101079989524b92bbd4dbf4",
+    "url": "/static/media/AEtest.2e2ee372.mp4"
+  },
+  {
+    "revision": "c72bed136e341f4c80a741fd983a1636",
+    "url": "/static/media/background1080.c72bed13.jpg"
+  },
+  {
+    "revision": "01b25038d576a914e7f3d8bdb2d7aaea",
+    "url": "/static/media/bg4.01b25038.jpg"
+  },
+  {
+    "revision": "56e1a61bf474b40478ac92263a71b140",
+    "url": "/static/media/work.56e1a61b.jpg"
+  }
+]);
